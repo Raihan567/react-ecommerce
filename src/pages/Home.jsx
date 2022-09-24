19f8398ab9db55168ff43b9bc7cb917724ca35aa
Raihan567 <aburaihansarker567@gmail.com>
2022-09-24 13:17:16 +0600

    productList Design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Announcement from "../Components/Announcement";
 import Categories from "../Components/Categories";
 import Footer from "../Components/Footer";
 import Navbar from "../Components/Navbar";
-import Newsletter from "../Components/Newslatter";
+import Newsletter from "../Components/Newsletter";
 import Products from "../Components/Products";
 import Slider from "../Components/Slider";
 
