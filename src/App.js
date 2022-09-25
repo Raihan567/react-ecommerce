8a627d6546b307419cac94c560d22686fe3001d3
Raihan567 <aburaihansarker567@gmail.com>
2022-09-25 16:17:44 +0600

    project complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 // import Product from "./pages/Product";
-import Cart from "./pages/Cart";
+// import Cart from "./pages/Cart";
 // import Login from "./pages/Login";
 // import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div>
-      <Cart />
+      <Home />
     </div>
   );
 }
